6f08ea98919ad93292a29c236ce986c9a1d3dafa
Atharva Maskar <atharva.maskar@choicetechlab.com>
2024-07-03 17:01:09 +0530

    Implemented fetching audio files from source url

---
+++ scripts/download_audio_files.py
@@ -0,0 +1,32 @@
+import requests
+
+def fetch_audio_files(audio_file_url: str) -> True:
+    """
+    Description:
+        - This function takes the audio file url, fetches the audio file and saves it in the data directoy
+
+    Parameters:
+        - audio_file_url (str): Url of the audio file
+
+    returns:
+        - result (bool): Returns true if the audio was saved successfully else returns false. 
+    """
+
+    # 1. fetch audio name 
+    audio_id = audio_file_url.split('/')[-1]
+
+    # 2. Performing fetch request 
+    url = audio_file_url
+    file_name = f"{audio_id}.mp3" 
+
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            with open(audio_id, 'wb') as audioFile:
+                audioFile.write(response.content)
+            return True
+        else:
+            print("Failed to download file. Status Code: ", response.status_code)                
+    except Exception as e:
+        print("Error fetching audio file from source: ", e)
